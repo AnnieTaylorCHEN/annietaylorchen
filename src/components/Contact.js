684f103ab7eab21fcc83e158fb32935292ee80ff
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-08-16 19:18:09 +0200

    fine-tune contact page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,18 +4,18 @@ export default function Contact () {
     return (
         <div>
             <div className="contact-header">
-                <h2 className="contact-header__title">Tell me about your projects.</h2>
+                <h2 className="contact-header__title">Tell me what you have in mind.</h2>
                 <a href="mailto:annietaylorchen@gmail.com">annie.taylor.chen@gmail.com</a>
             </div>
 
             <div className="contact">
                 <div className="contact-bg"></div>
                 <form className="contact-form" method="POST" action="https://formspree.io/annietaylorchen@gmail.com">
-                    <label>Name: (required)</label>
+                    <label>Name:</label>
                     <input type="text" name="name" placeholder="your name" required />
-                    <label>Email: (required)</label>
+                    <label>Email:</label>
                     <input type="email" name="email" placeholder="your email" required />
-                    <label>Request: (required)</label>
+                    <label>Request:</label>
                     <textarea name="message" type="text" placeholder="your request" rows="12" required></textarea>
                     <button className="send" type="submit">Send</button>
                 </form>

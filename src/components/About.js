a8654ccac1cb83f7cf2a18c52704e927854cc88c
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-08-18 17:47:04 +0200

    finish about page.

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,176 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,</p>
-        </div>
+        <>
+            <section className="about">
+                <div className="about__intro">
+                    Hi, I’m Annie. <br />
+                    I’m a <span className="highlight">full-stack developer</span> and <span className="highlight">photographer</span> <br />
+                    based in Stockholm, Sweden. 
+                </div>
+                <div className="about__details">
+                   <p>Although I didn’t study computer science in university and my previous career has not been programming related, I always strive the provide <span className="highlight">best service</span> for my clients. I like to help them <span className="highlight">solve problems</span>. Coding unleashes my creativity so I can build things I didn’t imagine I could do before. Now equipped with photography, graphic design and coding skills, I am ready to <span className="highlight">make a difference</span>. </p>
+
+                   <p>Besides coding and photography, I am also a <span className="highlight">vegan cook</span> and run a lifestyle blog <span className="highlight"><a href="https://petiteandminimal.com" target="_blank" rel="noopener noreferrer">Petite &amp; Minimal</a></span>. </p>
+                
+                   <p>Principles I believe in:</p>
+                </div>
+                <div className="about__principle principle1">
+                    <h3 className="highlight">Clean Code</h3>
+                    <p>Clean code requires thinking and planning, and it makes your future work less stressful</p>
+                </div>
+                <div className="about__principle principle2">
+                    <h3 className="highlight">Good UI/UX</h3>
+                    <p>In a fast-pacing world, we don't just need software, but good-looking and easy-to-use software </p>
+                </div>
+                <div className="about__principle principle3">
+                    <h3 className="highlight">Excellent Performance</h3>
+                    <p>You lose when you’re slow, optimization is a must-have </p>
+                </div>
+                <div className="about__principle principle4">
+                    <h3 className="highlight">Responsive</h3>
+                    <p>Works and looks great anywhere from phone to laptop</p>
+                </div>
+            </section>
+
+            <section className="skills">
+                <h3>Programming</h3>
+                <div className="skills__option">
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/html5.svg" alt="html5 logo"/>
+                        <p>Html 5</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/css3.svg" alt="css3 logo"/>
+                        <p>Css 3</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/javascript.svg" alt="javacript logo" />
+                        <p>Javascript</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/sass.svg" alt="sass logo" />
+                        <p>Sass</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/react.svg" alt="react logo" />
+                        <p>React</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/webpack.svg" alt="webpack logo" />
+                        <p>Webpack</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/wordpress.svg" alt="wordpress logo" />
+                        <p>Wordpress</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/woocommerce.svg" alt="woocommerce logo" />
+                        <p>WooCommerce</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/nodejs.svg" alt="nodejs logo" />
+                        <p>Node.js</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/mongodb.svg" alt="mongodb logo" />
+                        <p>MongoDB</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/postgresql.svg" alt="postgresql logo" />
+                        <p>PostgreSQL</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/firebase.svg" alt="firebase logo" />
+                        <p>Firebase</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/graphql.svg" alt="graphql logo" />
+                        <p>GraphQL</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/prisma.svg" alt="prisma logo" />
+                        <p>Prisma</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/apollo.svg" alt="apollo logo" />
+                        <p>Apollo</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/git.svg" alt="git logo" />
+                        <p>Git</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/linux-mint.svg" alt="linux-mint logo" />
+                        <p>Linux-Mint</p>
+                    </div>
+                </div>
+
+                <h3>UI, UX, Prototype</h3>
+                <div className="skills__option">
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/gimp.svg" alt="gimp logo" />
+                        <p>Gimp - open-source alternative to Photoshop</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/inkscape.svg" alt="inkscape logo" />
+                        <p>Inkscape - open-source alternative to Illustrator </p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/figma.svg" alt="figma logo" />
+                        <p>Figma</p>
+                    </div>
+                </div>
+
+                <h3>Project Management + Digital Marketing</h3>
+                <div className="skills__option">
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/jira.svg" alt="jira logo" />
+                        <p>JIRA</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/trello.svg" alt="trello logo" />
+                        <p>Trello</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/zenkit.png" alt="zenkit logo" />
+                        <p>Zenkit</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/slack.svg" alt="slack logo" />
+                        <p>Slack</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/seo.png" alt="seo logo" />
+                        <p>SEO</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/social.png" alt="social logo" />
+                        <p>Marketing strategy </p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/google-analytics.svg" alt="google-analytics logo" />
+                        <p>Google Analytics</p>
+                    </div>
+                </div>
+
+                <h3>Interested in learning more</h3>
+                <div className="skills__option">
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/python.svg" alt="python logo" />
+                        <p>Python</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/flutter.svg" alt="flutter logo" />
+                        <p>Flutter</p>
+                    </div>
+                    <div className="skills__item">
+                        <img className="skills__item__img" src="imgs/AI.png" alt="AI logo" />
+                        <p>AI</p>
+                    </div>
+                </div>
+            </section>
+                
+        </>
     )
 }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,27 +2,24 @@ import React, { useState } from 'react'
 
 export default function Profile() {
     
-    let topLayerWidth 
-    let handleLeft 
-    
     const [mouseMove, setMouseMove] = useState({
-      x: 0,
-      skew: 1000,
+      skew: 0,
       delta: 0,
-      topLayerWidth: 50,
-      handleLeft: 50 + '%'
+      topLayerWidth: 'calc(47vw + 1000px)',
+      handleLeft: 47 + '%'
     })
+     
 
     const handleMouseMove = (e) => {
       let delta = (e.clientX - window.innerWidth / 2) * 0.5
-      let skew = mouseMove.skew
-      console.log(delta)
+      let skew = 1000
+      
        setMouseMove({
+         skew: skew,
          delta: delta,
          topLayerWidth: e.clientX + skew + delta +'px',
          handleLeft: e.clientX + delta + 'px'
        })
-       console.log(topLayerWidth)
     }
 
     return (
@@ -34,24 +31,24 @@ export default function Profile() {
             <div className="layer bottom">
               <div className="content-wrap">
                 <div className="content-body">
-                  <h2>Full-Stack Developer</h2>
+                  <h2 className="content-body__title">Full-Stack Developer</h2>
                   <p>Building digital products with amazing UI/UX through clean code.</p> 
                 </div>
                 <img src="/imgs/1.jpg" alt="Annie Taylor CHEN - full-stack developer" />
               </div>
             </div>
 
-            <div className="layer top" style={{width: topLayerWidth }}>
+            <div className="layer top" style={{width: mouseMove.topLayerWidth }}>
               <div className="content-wrap">
                 <div className="content-body">
-                  <h2>Photographer</h2>
+                  <h2 className="content-body__title">Photographer</h2>
                   <p>Creating beautiful photos with design in mind.</p>
                 </div>
                 <img src="/imgs/2.jpg" alt="Annie Taylor CHEN - photographer" />
               </div>
             </div>
 
-            <div className="handle" style={{left: handleLeft}}></div>
+            <div className="handle" style={{left: mouseMove.handleLeft}}></div>
 
           </div>
         </>

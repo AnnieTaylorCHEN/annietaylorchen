2df6e779fe1b161b99fbf589ceafaf1dd5628b81
Annie Taylor CHEN <annietaylorchen@gmail.com>
2019-09-30 21:31:58 +0200

    add summary to projects.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,10 @@ export default function Projects() {
                     <Link to="/projects/ingersalternativ">
                         <img src="imgs/project-IA-cover.png" alt="project Ingers Alternativ cover"/>
                     </Link>
+                    <div className="project__summary">
+                        <p>A static website for a therapist.</p>
+                        <p>HTML, CSS, Javascript</p>
+                    </div>
                     <span className="project-overlay">
                         <Link to="/projects/ingersalternativ"><span>View More</span></Link>
                     </span>
@@ -20,6 +24,10 @@ export default function Projects() {
                     <Link to="/projects/meowlo" >
                         <img src="imgs/project-meowlo-cover.png" alt="project Meowlo cover" />
                     </Link>
+                    <div className="project__summary">
+                        <p>An MVC website for a cat start-up.</p>
+                        <p>HTML, CSS, Javascript, Node.js, Api</p>
+                    </div>
                     <span className="project-overlay">
                         <Link to="/projects/meowlo"><span>View More</span></Link>
                     </span>
@@ -29,6 +37,10 @@ export default function Projects() {
                     <Link to="/projects/thirtysecondstomars">
                         <img src="imgs/project-mars-cover.png" alt="project Thirty Seconds to Mars cover" />
                     </Link>
+                    <div className="project__summary">
+                        <p>A MERN website for band - promotion, e-commerce, community.</p>
+                        <p>HTML, CSS, Javascript, React, Redux, MongoDB, Node.js, Stripe</p>
+                    </div>
                     <span className="project-overlay">
                         <Link to="/projects/thirtysecondstomars" ><span>View More</span></Link>
                     </span>

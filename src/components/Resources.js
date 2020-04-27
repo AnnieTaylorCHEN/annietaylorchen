2fede85d3df1a401b3350059e9b62c843ea2f609
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-04-27 17:16:46 +0200

    add resources title.

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -15,7 +15,10 @@ const Tab = ({ children }) => {
   const { onClick, isActive } = useTabState()
 
   return (
-    <button className={isActive ? 'resource-btn is-active' : 'resource-btn' } onClick={onClick}>
+    <button
+      className={isActive ? 'resource-btn is-active' : 'resource-btn'}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
@@ -28,53 +31,56 @@ const Panel = ({ children }) => {
 }
 
 const Resources = () => (
-  <div className='resources-container'>
-    <Tabs>
-      <div className='tab-container'>
-        <Tab>Dev Magazines</Tab>
-        <Tab>Inspiration</Tab>
-        <Tab>Free Assets</Tab>
-        <Tab>Typography</Tab>
-        <Tab>Colors</Tab>
-        <Tab>Processing Tools</Tab>
-        <Tab>Dev Tools</Tab>
-        <Tab>Hosting</Tab>
-        <Tab>Javascript</Tab>
-        <Tab>Computer Science</Tab>
-      </div>
+  <>
+    <h2 className='resources-title'>Resources</h2>
+    <div className='resources-container'>
+      <Tabs>
+        <div className='tab-container'>
+          <Tab>Dev Magazines</Tab>
+          <Tab>Inspiration</Tab>
+          <Tab>Free Assets</Tab>
+          <Tab>Typography</Tab>
+          <Tab>Colors</Tab>
+          <Tab>Processing Tools</Tab>
+          <Tab>Dev Tools</Tab>
+          <Tab>Hosting</Tab>
+          <Tab>Javascript</Tab>
+          <Tab>Computer Science</Tab>
+        </div>
 
-      <Panel>
-        <DevMag />
-      </Panel>
-      <Panel>
-        <Inspiration />
-      </Panel>
-      <Panel>
-        <FreeAssets />
-      </Panel>
-      <Panel>
-        <Typography />
-      </Panel>
-      <Panel>
-        <Colors />
-      </Panel>
-      <Panel>
-        <ProcessingTools />
-      </Panel>
-      <Panel>
-        <DevTools />
-      </Panel>
-      <Panel>
-        <Hosting />
-      </Panel>
-      <Panel>
-        <Javascript />
-      </Panel>
-      <Panel>
-        <ComputerScience />
-      </Panel>
-    </Tabs>
-  </div>
+        <Panel>
+          <DevMag />
+        </Panel>
+        <Panel>
+          <Inspiration />
+        </Panel>
+        <Panel>
+          <FreeAssets />
+        </Panel>
+        <Panel>
+          <Typography />
+        </Panel>
+        <Panel>
+          <Colors />
+        </Panel>
+        <Panel>
+          <ProcessingTools />
+        </Panel>
+        <Panel>
+          <DevTools />
+        </Panel>
+        <Panel>
+          <Hosting />
+        </Panel>
+        <Panel>
+          <Javascript />
+        </Panel>
+        <Panel>
+          <ComputerScience />
+        </Panel>
+      </Tabs>
+    </div>
+  </>
 )
 
 export default Resources

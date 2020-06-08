d52c5199cc71e711982674b98996323c1b8f30c0
Annie Taylor CHEN <annietaylorchen@gmail.com>
2020-06-08 13:13:44 +0200

    add sitemap.

---
+++ src/sitemap-routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route } from 'react-router'
+
+export default (
+	<Route>
+		<Route exact path="/" />
+		<Route path="/about" />
+		<Route exact path="/projects" />
+		<Route path="/projects/ingersalternativ" />
+		<Route path="/projects/meowlo" />
+		<Route path="/projects/thirtysecondstomars" />
+		<Route path="/projects/petiteandminimalblog" />
+		<Route path="/projects/petiteandminimalstore" />
+		<Route path="/contact" />
+		<Route path="/resources" />
+	</Route>
+)
